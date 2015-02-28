136d3874dffcec27e521a8461f262436245a8cf8
corinne krych <corinnekrych@gmail.com>
2015-02-28 17:43:35 +0100

    Add photo browsing

--- Incognito/ViewController.swift
+++ Incognito/ViewController.swift
@@ -7,19 +7,68 @@
 //
 
 import UIKit
+import MobileCoreServices
+import AssetsLibrary
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var imagePicker = UIImagePickerController()
+    var newMedia: Bool = true
+    @IBOutlet weak var imageView: UIImageView!
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func showCamera(sender: AnyObject) {
+        showCamera()
+    }
+    
+    @IBAction func openCamera(sender: AnyObject) {
+        openPhoto()
+    }
+    
+    @IBAction func hide(sender: AnyObject) {
+    
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // MARK - UIImagePickerControllerDelegate
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    }
+    
+    // MARK - Private functions
+    
+    private func openPhoto() {
+        imagePicker.sourceType = UIImagePickerControllerSourceType.SavedPhotosAlbum
+        imagePicker.delegate = self
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    private func showCamera() {
+        if (UIImagePickerController.isSourceTypeAvailable(.Camera)) {
+            imagePicker.delegate = self
+            imagePicker.sourceType = .Camera
+            imagePicker.mediaTypes = NSArray(object: kUTTypeImage)
+            
+            self.presentViewController(imagePicker, animated:true, completion:{})
+            newMedia = true
+        } else {
+            
+        }
+    }
 
 }
 
